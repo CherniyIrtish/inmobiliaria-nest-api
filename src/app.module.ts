9e1f2837445cb65242b149c22c24f1e504983444
com <>
2025-09-14 15:20:15 +0200

    fix cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AppDataSource } from './database/data-source';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { ListingsModule } from './modules/listings/listings.module';
+import session from 'express-session';
 
 
 @Module({
@@ -25,17 +26,35 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     const isProd = process.env.NODE_ENV === 'production';
 
-    consumer
-      .apply(
-        cookieSession({
-          name: 'session',
-          keys: [this._configService.getOrThrow('COOKIE_KEY') as string],
-          maxAge: 1000 * 60 * 60 * 24 * 7,
-          httpOnly: false,
+    // consumer
+    //   .apply(
+    //     cookieSession({
+    //       name: 'session',
+    //       keys: [this._configService.getOrThrow('COOKIE_KEY') as string],
+    //       maxAge: 1000 * 60 * 60 * 24 * 7,
+    //       httpOnly: true,
+    //       secure: isProd,
+    //       sameSite: isProd ? 'none' : 'lax',
+    //     }),
+    //   )
+    //   .forRoutes({ path: '*', method: RequestMethod.ALL });
+
+    consumer.apply(
+      session({
+        name: 'session',
+        secret: process.env.COOKIE_KEY!,
+        resave: false,
+        saveUninitialized: false,
+        proxy: true,
+        cookie: {
+          httpOnly: true,
           secure: isProd,
           sameSite: isProd ? 'none' : 'lax',
-        }),
-      )
-      .forRoutes({ path: '*', method: RequestMethod.ALL });
+          partitioned: isProd ? true : (undefined as any),
+          maxAge: 7 * 24 * 60 * 60 * 1000,
+          path: '/',
+        } as any,
+      }),
+    ).forRoutes('*');
   }
 }
--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -4,7 +4,7 @@ import { CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/commo
 export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const req = context.switchToHttp().getRequest();
-        const isAuthed = !!req.session?.userId; // ЖЁСТКО в boolean
+        const isAuthed = !!req.session?.userId;
         if (!isAuthed) {
             throw new ForbiddenException('Not authenticated');
         }
