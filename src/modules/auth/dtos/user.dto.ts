e3c68f64264adad75807180868d946399c8bab21
com <>
2025-09-20 21:42:59 +0200

    add swagger

--- src/modules/auth/dtos/user.dto.ts
+++ src/modules/auth/dtos/user.dto.ts
@@ -1,24 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Expose, Type } from "class-transformer";
-import { ListingDto } from "src/modules/listings/dtos/listing-dto.dto";
+import { ListingDto } from "../../listings/dtos/listing-dto.dto";
 
 
 export class UserDto {
+    @ApiProperty({ example: 12 })
     @Expose()
     id: number;
 
+    @ApiProperty({ example: 'user@example.com' })
     @Expose()
     email: string;
 
+    @ApiProperty({ example: false })
     @Expose()
     admin: boolean;
 
+    @ApiProperty({ example: true })
     @Expose()
     totpEnabled: boolean;
 
+    @ApiProperty({ example: false })
     @Expose()
     totpRequired: boolean;
 
-
+    @ApiProperty({ type: () => [ListingDto], required: false })
     @Expose()
     @Type(() => ListingDto)
     listings: ListingDto[];
