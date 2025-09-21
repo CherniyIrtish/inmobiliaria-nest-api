79f22ce4c58f7623197496f807e023c6d2d61e41
com <>
2025-09-21 02:12:42 +0200

    update readme

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ npm run start:dev
 
 # prod (postgres)
 
+Remove all local migrations
+
+export NODE_ENV=production
+export DATABASE_URL='postgres://user:pass@localhost:5432/mydb'
+
 npm run build
 npm run migration:generate:prod
 npm run migration:run:prod
---
+++ src/database/migrations/1758413191868-AutoProd.ts
@@ -0,0 +1,26 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AutoProd1758413191868 implements MigrationInterface {
+    name = 'AutoProd1758413191868'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "listings" DROP CONSTRAINT "FK_45d5c4642c4cad0229da0ec22e7"`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "totpEnabled" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "totpSecretEnc" text`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "totpVerifiedAt" TIMESTAMP WITH TIME ZONE`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "tokenVersion" integer NOT NULL DEFAULT '0'`);
+        await queryRunner.query(`ALTER TABLE "users" ADD "totpRequired" boolean NOT NULL DEFAULT false`);
+        await queryRunner.query(`ALTER TABLE "listings" ADD CONSTRAINT "FK_45d5c4642c4cad0229da0ec22e7" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "listings" DROP CONSTRAINT "FK_45d5c4642c4cad0229da0ec22e7"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "totpRequired"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "tokenVersion"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "totpVerifiedAt"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "totpSecretEnc"`);
+        await queryRunner.query(`ALTER TABLE "users" DROP COLUMN "totpEnabled"`);
+        await queryRunner.query(`ALTER TABLE "listings" ADD CONSTRAINT "FK_45d5c4642c4cad0229da0ec22e7" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+    }
+
+}
